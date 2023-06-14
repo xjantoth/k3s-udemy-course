0a4cf2f292f51f3e96beeba510f086e14664c773
Jan Toth <devopsinuse@gmail.com>
2023-06-14 15:34:46 +0200

    Wokring SSM version - TODO: wait for Nginx Ingress Controller

--- config/node.sh
+++ config/node.sh
@@ -27,19 +27,21 @@ done
 
 export K3S_TOKEN_SSM=$(aws ssm get-parameters --names k3s_token --query 'Parameters[0].Value' --output text --region ${REGION})
 curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_PRIVATE_IPV4}:6443 K3S_TOKEN=$K3S_TOKEN_SSM sh -
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-# Install helm binary
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
 
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -F __start_kubectl k' >>~/.bashrc
-
-echo 'source <(kubectl completion bash)' >>/home/ubuntu/.bashrc
-echo 'alias k=kubectl' >>/home/ubuntu/.bashrc
-echo 'complete -F __start_kubectl k' >>/home/ubuntu/.bashrc
-source ~/.bashrc
+# export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+#
+# # Install helm binary
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# chmod 700 get_helm.sh
+# ./get_helm.sh
+#
+# echo 'source <(kubectl completion bash)' >>~/.bashrc
+# echo 'alias k=kubectl' >>~/.bashrc
+# echo 'complete -F __start_kubectl k' >>~/.bashrc
+#
+# echo 'source <(kubectl completion bash)' >>/home/ubuntu/.bashrc
+# echo 'alias k=kubectl' >>/home/ubuntu/.bashrc
+# echo 'complete -F __start_kubectl k' >>/home/ubuntu/.bashrc
+# source ~/.bashrc
 
